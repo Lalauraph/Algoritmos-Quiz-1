b221ac46ca8313c471a0736a042ce957e2fbc92b
Lalauraph <laurapalacio900@gmail.com>
2024-02-09 17:42:39 -0500

    quiz

---
+++ index.js
@@ -0,0 +1,15 @@
+//Arreglo
+const studentGrades = [100, 50, 65, 70, 85, 96, 45];
+
+//Función
+const computeGrades = (arreglo) => {
+	arreglo.forEach((studentGrade) => {
+		if (studentGrade < 60) studentGrade = F;
+		else if (studentGrade > 60 < 69) studentGrade = D;
+		else if (studentGrade > 70 < 79) studentGrade = C;
+	});
+};
+
+//Llamar a la función
+const result = computeGrades(studentGrades);
+console.log(result);
