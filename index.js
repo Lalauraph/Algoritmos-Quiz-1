2b485c48221734eca54908a1c83d747293f1bcca
Lalauraph <laurapalacio900@gmail.com>
2024-02-09 17:50:23 -0500

    quiz

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const studentGrades = [100, 50, 65, 70, 85, 96, 45];
 const computeGrades = (arreglo) => {
 	arreglo.forEach((studentGrade) => {
 		if (studentGrade < 60) {
-			studentGrade = F;
+			(studentGrade = F), lose;
 		} else if (studentGrade > 60 < 69) {
 			studentGrade = D;
+			passed;
 		} else if (studentGrade > 70 < 79) {
 			studentGrade = C;
+			passed;
 		} else if (studentGrade > 80 < 89) {
 			studentGrade = B;
+			passed;
 		} else if (studentGrade > 90 < 100) {
 			studentGrade = A;
+			passed;
 		}
 	});
 	return studentGrades;
